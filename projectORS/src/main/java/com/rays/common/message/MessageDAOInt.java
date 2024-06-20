39e358ca59e53f1a690037cbfdaac713b49666aa
Hardeepsiddhu <hardeepsiddhu2001@gmail.com>
2024-06-20 16:13:33 -0700

    doneee

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Hardeep Siddhu 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
